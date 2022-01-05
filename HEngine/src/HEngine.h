84dce841cb4792d58cf3209d6921a2753f228049
debyecao <debyecao@outlook.com>
2022-01-05 14:41:51 +0800

    add spdlog as submodule to be logger system

--- HEngine/src/HEngine.h
+++ HEngine/src/HEngine.h
@@ -4,6 +4,7 @@
 
 #include <cstdio>
 #include "HEngine/Application.h"
+#include "HEngine/Log.h"
 
 // -------Entry Point----------
 #include "HEngine/EntryPoint.h"
--- HEngine/src/HEngine/EntryPoint.h
+++ HEngine/src/HEngine/EntryPoint.h
@@ -6,11 +6,13 @@ extern HEngine::Application* HEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hello HEngine!");
+	HEngine::Log::Init();
+	HE_CORE_WARN("Init Finish!");
+	HE_INFO("TEST! Var = {0}", 123);
+
 	auto app = HEngine::CreateApplication();
 	app->Run();
 	delete app;
 }
 
 #endif
-
